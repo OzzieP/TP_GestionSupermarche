c490735371ffe4b4d62d49d0487c11537f2a0994
Brendan LEGENDRE <brendan.legendre@epsi.fr>
2020-02-26 09:46:12 +0100

    Création des classes.

---
+++ src/Article.java
@@ -0,0 +1,61 @@
+public class Article {
+    long codeBarre;
+    String reference;
+    String libelle;
+    int prixHT;
+    int tauxTVA;
+    public static int[] tableauTVA = { 550, 2000 };
+
+    public long getCodeBarre() {
+        return codeBarre;
+    }
+
+    public void setCodeBarre(long codeBarre) {
+        this.codeBarre = codeBarre;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public int getPrixHT() {
+        return prixHT;
+    }
+
+    public void setPrixHT(int prixHT) {
+        this.prixHT = prixHT;
+    }
+
+    public int getTauxTVA() {
+        return tauxTVA;
+    }
+
+    public void setTauxTVA(int tauxTVA) {
+        this.tauxTVA = tauxTVA;
+    }
+
+    public static int[] getTableauTVA() {
+        return tableauTVA;
+    }
+
+
+    public Article(long codeBarre, String reference, String libelle, int prixHT, int tauxTVA) {
+        this.codeBarre = codeBarre;
+        this.reference = reference;
+        this.libelle = libelle;
+        this.prixHT = prixHT;
+        this.tauxTVA = tauxTVA;
+    }
+}
---
+++ src/Utilisateur.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Utilisateur {
+    String pseudo;
+    ArrayList<Article> panier;
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public ArrayList<Article> getPanier() {
+        return panier;
+    }
+
+    public void setPanier(ArrayList<Article> panier) {
+        this.panier = panier;
+    }
+
+    public Utilisateur(String pseudo) {
+        this.pseudo = pseudo;
+        this.panier = new ArrayList<>();
+    }
+}
